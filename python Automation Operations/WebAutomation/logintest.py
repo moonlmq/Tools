73a14e09060d49f3f01b256fd76214ce07731404
moonlmq <545427522@qq.com>
2016-07-07 22:12:33 +0800

    web login
    selenium

---
+++ python Automation Operations/WebAutomation/logintest.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import time,os
+from selenium.webdriver.common.keys import Keys
+
+chromedriver = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
+os.environ["webdriver.chrome.driver"]=chromedriver
+b = webdriver.Chrome(chromedriver)
+
+# b.get("http://fhnw.fiberhome.com/")
+# #time.sleep(3)
+# #b.find_element_by_id("CCM_LoginMain1_tbName").clear
+# time.sleep(1)
+# b.find_element_by_id("CCM_LoginMain1_tbName").focus()
+# b.find_element_by_id("CCM_LoginMain1_tbName").send_keys("mqliang")
+# time.sleep(1)
+# #b.find_element_by_id("CCM_LoginMain1_tbPass").clear
+# #time.sleep(1)
+# b.find_element_by_id("CCM_LoginMain1_tbPass").send_keys("moonlmq")
+# b.find_element_by_id("CCM_LoginMain1_ImageButtonlogin1").click()
+# time.sleep(2)
+# elem= b.find_element_by_id("IB2").click()
+# time.sleep(5)
+# b.close()
+
+b.get("http://www.baidu.com/")
+
+time.sleep(1)
+
+b.find_element_by_id("kw").clear
+
+time.sleep(1)
+
+b.find_element_by_id("kw").send_keys("hello")
