57f406407c1dafbb1780175fcb2b4dfac4a1fa2c
moonlmq <545427522@qq.com>
2016-07-25 17:38:46 +0800

    debugger

--- GHP/my_debugger.py
+++ GHP/my_debugger.py
@@ -1,3 +1,4 @@
+#-*-coding:utf-8 -*-
 from ctypes import *
 from my_debugger_defines import *
 kernel32 = windll.kernel32
@@ -8,50 +9,50 @@ class debugger():
 		self.h_process = 	None
 		self.pid =			None
 		self.debugger_active = False
+		self.h_thread        =     None
+		self.context         =     None
 
 	def load(self,path_to_exe):
-		# dwCreation flag determines how to create the process
-		# set creation_flags = CREATE_NEW_CONSOLE if want to
-		# see the GUI
+		# dwCreation flag 标志位控制进程的创建方式，若希望新创建的进程独占一个新的
+		#控制台窗口，而不是与父进程共用同一个控制台，可以加上标志位CREATE_NEW_CONSOLE
 		creation_flags = DEBUG_PROCESS
-		#instantiate the structs
+		#实例化结构体
 		startupinfo = STARTUPINFO()
 		process_information = PROCESS_INFORMATION()
-		# The following two options allow the started process
-		# to be shown as a separate window. This also illustrates
-		# how different settings in the STARTUPINFO struct can affect
-		# the debuggee.
+		# 以下两个变量的共同作用下，新建进程将在一个单独的窗体中被显示，可以通过改变
+		#STARTUPINFO中的各变量来控制debugee进程的行为
 		startupinfo.dwFlags = 0x1
 		startupinfo.wShowWindow = 0x0
 
-		# Then initialize the cb variable in the STARTUPINFO struct
-		# which is just the size of the struct itself
+		# 设置结构体STARTUPINFO中的成员变量cb的值，用以标志结构体本身的大小
 		startupinfo.cb = sizeof(startupinfo)
+		#在windows下创建一个新进程的工作可以由CreateProcessA完成
 		if kernel32.CreateProcessA(path_to_exe,
 			None,
 			None,
 			None,
 			None,
-			creation_flags,
+			creation_flags,#开启调试功能
 			None,
 			None,
-			byref(startupinfo),
-			byref(process_information)):
+			byref(startupinfo),#指定进程的启动方式
+			byref(process_information)):#记录进程成功启动后相关状态信息
 			print ("[*] The process have been successfully launched!")
 			print ("[*] PID: %d" % process_information.dwProcessId)
 
 			# Obtain a valid handle to the newly created process
 			# and store it for future acccess
+			self.pid = process_information.dwProcessId
 			self.h_process=self.open_process(process_information.dwProcessId)
-			return process_information.dwProcessId
 		else:
 			print ("[*] Error: 0x%08x." % kernel32.GetLastError())
-			return 0
+			
 
-
+	#获取进程句柄
 	def open_process(self,pid):
 		print ("into open process")
 		PROCESS_ALL_ACCESS =2035711
+		#OpenProcess可以获取句柄
 		h_process=kernel32.OpenProcess(PROCESS_ALL_ACCESS,False,pid)
 		print ("the h_process is "+ str(h_process))
 		return h_process
@@ -59,14 +60,11 @@ class debugger():
 	def attach(self,pid):
 		self.h_process = self.open_process(pid)
 		print ("attach h_process is "+ str(self.h_process))
-		# Attempt to attach to the process
-		#if this fails  exit the call
-		print (kernel32.DebugActiveProcess(pid))
+		#实现进程附加
 		if (kernel32.DebugActiveProcess(pid)):
 			self.debugger_active = True
 			self.pid = int(pid)
-			print ("ok")
-			self.run()
+			print ("attach ok")
 		else:
 			print ("[*] Unable to attach to the process")
 			print("[*] Errorcode:0x%08x." % kernel32.GetLastError())  
@@ -79,16 +77,31 @@ class debugger():
 	def get_debug_event(self):
 		debug_event = DEBUG_EVENT()
 		continue_status = DBG_CONTINUE
+		#waitfordebugevent收集发生在目标进程中的调试事件
+		#第一个参数指向结构体DEBUG_EVENT,用于描述一个具体的调试事件
+		#第二个参数表示等待下一个调试事件发生的时间上限
 		if kernel32.WaitForDebugEvent(byref(debug_event),INFINITE):
 			# Not going to build any event handlers
 			# just yet. Just resume the process for now.
-			raw_input("Press a key to continue...")
-			self.debugger_active = False
+			# raw_input("Press a key to continue...")
+			# self.debugger_active = False
+			self.h_thread          = self.open_thread(debug_event.dwThreadId)
+			self.context           = self.get_thread_context(h_thread=self.h_thread)
+			self.debug_event       = debug_event
+			print "Event Code: %d Thread ID: %d" %\
+			(debug_event.dwDebugEventCode,debug_event.dwThreadId)
+			#将目标进程恢复至原来的执行状态
+			#第一二个参数来源于DEBUG_EVENT的同名成员变量
+			#第三个参数决定目标进程是继续执行（DBG_CONTINUE)还是继续处理
+			#所捕获的异常事件（DBG_EXECPTION_NOT_HANDLED
+
+			
 			kernel32.ContinueDebugEvent(debug_event.dwProcessId,
 				debug_event.dwThreadId,continue_status)
 
+	#DebugActiveProcessStop可以使调试器与被调试进程分离
 	def detach(self):
-		if kernel32.DebugActiveProcess(self.pid):
+		if kernel32.DebugActiveProcessStop(self.pid):
 			print ("[*] Finished debugging. Exiting...")
 			return True
 		else:
@@ -97,6 +110,7 @@ class debugger():
 
 
 	def open_thread(self,thread_id):
+		#线程句柄可由OpenThread函数取的
 		h_thread=kernel32.OpenThread(THREAD_ALL_ACCESS,None,thread_id)
 		if h_thread is not None:
 			return h_thread
@@ -108,31 +122,39 @@ class debugger():
 
 		thread_entry = THREADENTRY32()
 		thread_list = []
-		snapshot = kernel32.CreateToolhelp32Snapshot(TH32CS_SNAPTH READ,self.pid)
+		#CreateToolhelp32Snapshot函数可以获取系统进程列表，系统中的
+		#线程列表，被加载人某一进程的所有模块（DLLs）列表以及
+		#某一进程所属堆列表
+		#第一个参数用于告知希望获取信息的确切类型（线程列表，进程列表，模块列表还是堆列表）
+		snapshot = kernel32.CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD,self.pid)
 
 		if snapshot is not None:
 			thread_entry.dwSize = sizeof(thread_entry)
 			success = kernel32.Thread32First(snapshot,byref(thread_entry))
 
+
+
 			while success:
+				#筛选目前进程下的线程
 				if thread_entry.th32OwnerProcessID == self.pid:
 					thread_list.append(thread_entry.th32ThreadID)
-					success = kernel32.Thread32Next(snapshot,byref(thread_entry))
-					kernel32.CloseHandle(snapshot)
-					return thread_list
-				else:
-					return False
+				#获取下一个线程数据
+				success = kernel32.Thread32Next(snapshot,byref(thread_entry))
+			kernel32.CloseHandle(snapshot)
+			return thread_list
+		else:
+			return False
 
 	def get_thread_context(self,thread_id=None,h_thread=None):
 		context =CONTEXT()
 		context.ContextFlags = CONTEXT_FULL | CONTEXT_DEBUG_REGISTERS
-
+		#获取线程句柄
 		h_thread = self.open_thread(thread_id)
+		#从线程中提取上下文环境信息
 		if kernel32.GetThreadContext(h_thread,byref(context)):
 			kernel32.CloseHandle(h_thread)
 			return context
 		else:
 			return False
-
 
 
