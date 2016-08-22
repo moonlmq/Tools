d73d5c4150abb4d44f09db9cab51634a36035054
moonlmq <545427522@qq.com>
2016-08-22 16:49:34 +0800

    opencv

---
+++ Machine Learning/Computer Vision/testCameraImage.py
@@ -0,0 +1,14 @@
+import cv2
+
+cameraCapture = cv2.VideoCapture(0)
+fps = 30 # an assumption
+size = (int(cameraCapture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),\
+	int(cameraCapture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
+videoWriter = cv2.VideoWriter('Output.avi',cv2.cv.CV_FOURCC('I','4','2','0'),fps,size)
+
+success, frame = cameraCapture.read()
+numFramesRemaining = 10 *fps - 1
+while success and numFramesRemaining >0:
+	VideoWriter.write(frame)
+	success, frame = cameraCapture.read()
+	numFramesRemaining -= 1t
